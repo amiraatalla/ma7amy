73031cb6c8b2f4853d585b9c7c42064759f5761a
amiraatalla <amiraatalla63@gmail.com>
2024-01-10 01:37:15 +0200

    :sparkles: add a todo

---
+++ src/components/Form.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+
+function Form({ input, setInput, todos, setTodos }) {
+   const onInputChange = (event) => {
+      setInput(event.target.value);
+   };
+
+   const onFormSubmit = (event) => {
+      event.preventDefault();
+      setTodos([...todos, {
+         id: uuidv4(),
+         title: input,
+         completed: false
+      }]);
+      setInput("");
+   };
+
+   return (
+      <div >
+         <form onSubmit={onFormSubmit}>
+            <input type="text" placeholder="Enter your task" className="task-input" value={input} required onChange={onInputChange} />
+            <button className="button-add" type="submit">Add</button>
+         </form>
+      </div>
+   );
+}
+
+export default Form;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Header() {
+  return (
+   
+     <div className="header">
+        <h1>Ma7amy</h1>  
+     </div>  
+  );
+}
+
+export default Header;
